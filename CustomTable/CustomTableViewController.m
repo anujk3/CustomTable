208dd13a86a801c1dfce94773c4d278499017166
Anuj Katiyal <anujk3@gmail.com>
2014-09-06 14:18:07 +0530

    Got this app working like the previous SimpleTable Example.

--- CustomTable/CustomTableViewController.m
+++ CustomTable/CustomTableViewController.m
@@ -12,18 +12,48 @@
 
 @end
 
-@implementation CustomTableViewController
+@implementation CustomTableViewController{
+    NSArray *recipeNames;
+    
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    recipeNames = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    
+    [self.tableView setContentInset:UIEdgeInsetsMake(20, self.tableView.contentInset.left,
+                                                     self.tableView.contentInset.bottom, self.tableView.contentInset.right)];
+
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return [recipeNames count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    static NSString *CellIdentifier = @"CustomTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                      reuseIdentifier:CellIdentifier];
+    }
+    
+    cell.textLabel.text = [recipeNames objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+    return cell;
 }
 
 @end
