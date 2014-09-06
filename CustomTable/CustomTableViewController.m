035992eaedf4806add94827dbce77936353640ff
Anuj Katiyal <anujk3@gmail.com>
2014-09-06 14:27:40 +0530

    Different thumbnails for different table rows

--- CustomTable/CustomTableViewController.m
+++ CustomTable/CustomTableViewController.m
@@ -14,6 +14,7 @@
 
 @implementation CustomTableViewController{
     NSArray *recipeNames;
+    NSArray *recipeImages;
     
 }
 
@@ -23,6 +24,9 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     recipeNames = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    
+    recipeImages = [NSArray arrayWithObjects:@"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg",
+                    @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
     
     [self.tableView setContentInset:UIEdgeInsetsMake(20, self.tableView.contentInset.left,
                                                      self.tableView.contentInset.bottom, self.tableView.contentInset.right)];
@@ -52,7 +56,7 @@
     }
     
     cell.textLabel.text = [recipeNames objectAtIndex:indexPath.row];
-    cell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+    cell.imageView.image = [UIImage imageNamed:[recipeImages objectAtIndex:indexPath.row]];
     return cell;
 }
 
