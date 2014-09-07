6ca8f9c453e219990c0e735d1054d17cc60f2fb4
Anuj Katiyal <anujk3@gmail.com>
2014-09-07 16:39:54 +0530

    Create a recipe object to hold to recipe values instead of using the arrays

---
+++ CustomTable/Recipe.m
@@ -0,0 +1,13 @@
+//
+//  Recipe.m
+//  CustomTable
+//
+//  Created by Anuj Katiyal on 07/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import "Recipe.h"
+
+@implementation Recipe
+
+@end
