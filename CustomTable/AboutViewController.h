a47163b9caab4d2368b665b771cf7a5f1d4ecf3e
Anuj Katiyal <anujk3@gmail.com>
2014-09-07 05:37:26 +0530

    UIWebView - How to display elements in a web view by passing URL request to either local or remote http links

---
+++ CustomTable/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  CustomTable
+//
+//  Created by Anuj Katiyal on 07/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@property (nonatomic, strong) IBOutlet UIWebView *webView;
+
+@end
